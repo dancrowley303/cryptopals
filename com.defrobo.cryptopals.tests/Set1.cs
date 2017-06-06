628644f99ed0b44ea20561391e959c21bd092388
Dan Crowley <defrobo@outlook.com>
2017-06-06 21:42:00 +0900

    solved challenge 2 set 1: Fixed XOR

--- com.defrobo.cryptopals.tests/Set1.cs
+++ com.defrobo.cryptopals.tests/Set1.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Text;
 
 namespace com.defrobo.cryptopals.tests
 {
@@ -10,6 +11,15 @@ namespace com.defrobo.cryptopals.tests
         {
             string result = Crypto.HexStringToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
             Assert.AreEqual("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t", result);
+        }
+
+        [Test]
+        public void Challenge2()
+        {
+            byte[] left = Crypto.HexStringToByteArray("1c0111001f010100061a024b53535009181c");
+            byte[] right = Crypto.HexStringToByteArray("686974207468652062756c6c277320657965");
+            var result = Crypto.FixedXOR(left, right);
+            Assert.AreEqual("the kid don't play", Encoding.UTF8.GetString(result));
         }
     }
 }
--- com.defrobo.cryptopals/Crypto.cs
+++ com.defrobo.cryptopals/Crypto.cs
@@ -14,13 +14,26 @@ namespace com.defrobo.cryptopals
             }
         }
 
+        public static byte[] FixedXOR(byte[] left, byte[] right)
+        {
+            if (left.Length != right.Length)
+                throw new ArgumentException("left and right parameters must be the same length");
+
+            var xored = new byte[left.Length];
+            for (var i = 0; i < left.Length; i++)
+            {
+                xored[i] = (byte)(left[i] ^ right[i]);
+            }
+            return xored;
+        }
+
         public static string HexStringToBase64(string input)
         {
             var hexSplit = HexStringToByteArray(input);
             return Convert.ToBase64String(hexSplit);
         }
 
-        private static byte[] HexStringToByteArray(string input)
+        public static byte[] HexStringToByteArray(string input)
         {
             return input.SplitInParts(2)
                  .Select(s => (byte)Convert.ToInt32(s, 16))
