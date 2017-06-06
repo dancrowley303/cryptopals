794268ac917c9441f825ee8b930b2eb800316fb6
Dan Crowley <defrobo@outlook.com>
2017-06-06 21:28:04 +0900

    solved challenge 1 set 1: Convert hex to base64

---
+++ com.defrobo.cryptopals/Crypto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace com.defrobo.cryptopals
+{
+    public static class Crypto
+    {
+        public static IEnumerable<String> SplitInParts(this String s, int partLength)
+        {
+            for (var i = 0; i < s.Length; i+=partLength)
+            {
+                yield return s.Substring(i, Math.Min(partLength, s.Length - i));
+            }
+        }
+
+        public static string HexStringToBase64(string input)
+        {
+            var hexSplit = HexStringToByteArray(input);
+            return Convert.ToBase64String(hexSplit);
+        }
+
+        private static byte[] HexStringToByteArray(string input)
+        {
+            return input.SplitInParts(2)
+                 .Select(s => (byte)Convert.ToInt32(s, 16))
+                 .ToArray();        
+        }
+    }
+}
